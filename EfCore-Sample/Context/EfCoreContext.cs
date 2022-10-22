95cad4d0f3b5cf79946ffe29ee37ba495e29be81
Mohamad Reza Amoori <mohamad.reza.amoori99@gmail.com>
2022-10-22 18:15:34 +0330

    Initial Model & Context Class & AppSettings

---
+++ EfCore-Sample/Context/EfCoreContext.cs
@@ -0,0 +1,15 @@
+﻿using EfCore_Sample.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EfCore_Sample.Context
+{
+    public class EfCoreContext : DbContext
+    {
+        public DbSet<Person> MyProperty { get; set; }
+
+        public EfCoreContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ EfCore-Sample/Models/Person.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EfCore_Sample.Models
+{
+    public class Person
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public Person(string userName, string password)
+        {
+            UserName = userName;
+            Password = password;
+        }
+    }
+}
