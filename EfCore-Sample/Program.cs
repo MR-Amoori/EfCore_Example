24eba92d9a40608169a8a7623e84dd88861341f3
Mohamad Reza Amoori <mohamad.reza.amoori99@gmail.com>
2022-10-22 18:26:31 +0330

    Add Context In Program.cs Class

--- EfCore-Sample/Program.cs
+++ EfCore-Sample/Program.cs
@@ -1,7 +1,27 @@
+using EfCore_Sample.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 
+#region Context
+
+// This ConnectionString : 
+var connectionString = builder.Configuration.GetConnectionString("LocalHostConnection");
+// Or
+var connectionString2 = builder.Configuration.GetSection("connectionString")["LocalHostConnection"];
+// End ConnectionString
+
+// --------------------
+
+// This Add Context 
+
+builder.Services.AddDbContext<EfCoreContext>(x => x.UseSqlServer(connectionString));
+
+// End Add Context
+
+#endregion
 
 
 var app = builder.Build();
