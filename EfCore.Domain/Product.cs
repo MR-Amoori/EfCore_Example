2b191214c55dbdf04e024f9a3997b9ace1c64ee7
Mohamad Reza Amoori <mohamad.reza.amoori99@gmail.com>
2022-10-25 00:13:17 +0330

    Add Domain Models (Product, PoductCategory)

---
+++ EfCore.Domain/Product.cs
@@ -0,0 +1,38 @@
+﻿namespace EfCore.Domain
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double UnitPrice { get; set; }
+        public bool IsRemoved { get; set; }
+
+        public int CategortId { get; set; }
+        public ProductCategory Category { get; set; }
+
+        public Product(string name, double unitPrice, int categortId)
+        {
+            Name = name;
+            UnitPrice = unitPrice;
+            IsRemoved = false;
+            CategortId = categortId;
+        }
+
+        public void Edit(string name, double unitPrice, int categortId)
+        {
+            Name = name;
+            UnitPrice = unitPrice;
+            CategortId = categortId;
+        }
+
+        public void Delete()
+        {
+            this.IsRemoved = true;
+        }
+
+        public void Restore()
+        {
+            this.IsRemoved = false;
+        }
+    }
+}
---
+++ EfCore.Domain/ProductCategory.cs
@@ -0,0 +1,20 @@
+﻿namespace EfCore.Domain
+{
+    public class ProductCategory
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Product> Products { get; set; }
+
+        public ProductCategory(string name)
+        {
+            Name = name;
+            Products = new List<Product>();
+        }
+
+        public void Edit(string name)
+        {
+            Name = name;
+        }
+    }
+}
